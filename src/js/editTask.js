7c8ee846c48435e8d30f29f9564b8756c3d17778
Ryotruuser <ryonantruuser@gmail.com>
2023-11-26 21:51:26 -0300

    now the tasks can be edit by the user

--- src/js/editTask.js
+++ src/js/editTask.js
@@ -4,7 +4,7 @@ let tasksAvaliables = []
 
 //variables to get the index of task inside url
 let url = window.location.href
-let pid = url.split("?")[1]
+let pid = parseInt(url.split("?")[1])
 
 //adding the task in the array
 for(let i = 1; i < storageTaskLen + 1; i++ ){
@@ -16,17 +16,31 @@ let taskInputName = document.getElementById("task-name-edit")
 let taskInputTime = document.getElementById("task-edit-time")
 let taskInputDate = document.getElementById("task-edit-date")
 let taskInputDesc = document.getElementById("task-edit-desc")
-
+//fill the fields with the storage info
 taskInputName.value = tasksAvaliables[pid].taskName
 taskInputTime.value = tasksAvaliables[pid].taskTime
 taskInputDate.value = tasksAvaliables[pid].taskDate
 taskInputDesc.value = tasksAvaliables[pid].taskDesc
+
+//test
 
 //actions btn
 let returnToMainApp = () =>{
     window.location.href = "../../index.html"
 }
 let editTask = () =>{
-    console.log(tasksAvaliables[pid])
-    
-}
+    let editAlert = document.getElementById("task-edit-alerts")
+    if(taskInputName.value != "" && taskInputTime.value != "" && taskInputDate.value != ""){
+        let task = {
+            "taskName" : taskInputName.value,
+            "taskTime" : taskInputTime.value,
+            "taskDate" : taskInputDate.value,
+            "taskDesc" : taskInputDesc.value
+        }
+        localStorage.setItem(pid + 1, JSON.stringify(task))
+    }else{
+        editAlert.style.color = "red"
+        editAlert.innerHTML = "empty input detected"
+        setTimeout(function() { editAlert.innerHTML = "" }, 2000);
+    }
+} 
